5a0a6bc3d64c2d863c04c4737430655daa733373
Srinu Lade <SrinuL@Srinus-MacBook-Pro.local>
2017-03-31 15:24:12 -0400

    Disables Log In and Sign Up buttons if no input is given

--- src/com/slackers/inc/Boundary/BoundaryControllers/ApplicationsController.java
+++ src/com/slackers/inc/Boundary/BoundaryControllers/ApplicationsController.java
@@ -1,5 +1,8 @@
 package com.slackers.inc.Boundary.BoundaryControllers;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,6 +12,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Modality;
@@ -21,20 +26,23 @@ import java.util.ResourceBundle;
 public class ApplicationsController implements Initializable{
 
     @FXML Accordion accordionID;
+    @FXML private BorderPane helloThing;
+    @FXML TitledPane title1;
+    private DoubleProperty width = new SimpleDoubleProperty(500);
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("This is the Application Controller");
 
-        for (Node node : accordionID.getPanes()) {
-            System.out.println("Id: " + node.getClass());
-            
-        }
+
+        //width.bind(accordionID.widthProperty());
+        //helloThing.styleProperty().bind(Bindings.concat("-fx-pref-width: ", width.subtract(50).asString(), ";"));
 
     }
 
     @FXML
     public void addApplication() throws IOException {
+
         Parent newApp = FXMLLoader.load(getClass().getResource("../FXML/form.fxml"));
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
