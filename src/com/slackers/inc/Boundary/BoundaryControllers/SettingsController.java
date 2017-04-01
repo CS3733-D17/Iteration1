d6bc900701c9af84b46a8784e97f1aa81a6c1c89
LEEDLE_MACHINE\Jason <jabel@wpi.edu>
2017-04-01 15:22:45 -0400

    sending database cuz reasons

--- src/com/slackers/inc/Boundary/BoundaryControllers/SettingsController.java
+++ src/com/slackers/inc/Boundary/BoundaryControllers/SettingsController.java
@@ -1,7 +1,43 @@
 package com.slackers.inc.Boundary.BoundaryControllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 /**
  * Created by Jason on 4/1/2017.
  */
 public class SettingsController {
+
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private TextField oldPassword;
+
+    @FXML
+    private TextField newPassword;
+
+    @FXML
+    private TextField email;
+
+    public MainController main;
+
+    @FXML
+    void UpdatePersonal(ActionEvent event) {
+        //main.getUser();
+    }
+
+    @FXML
+    void UpdatePassword(ActionEvent event) {
+
+    }
 }
