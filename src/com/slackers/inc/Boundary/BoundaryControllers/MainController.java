9493dc13841c32c37ebf5bfe5aafd212819d7e4e
Srinu Lade <SrinuL@Srinus-MacBook-Pro.local>
2017-04-01 15:00:46 -0400

    Started breaking up stuff

--- src/com/slackers/inc/Boundary/BoundaryControllers/MainController.java
+++ src/com/slackers/inc/Boundary/BoundaryControllers/MainController.java
@@ -1,6 +1,7 @@
 package com.slackers.inc.Boundary.BoundaryControllers;
 
 import com.slackers.inc.Controllers.AccountController;
+import com.slackers.inc.database.entities.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,18 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import javax.sound.midi.SysexMessage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
 
-
 public class MainController implements Initializable{
 
     private Preferences programPref;
     private AccountController userController;
+    private User user;
 
     @FXML private AnchorPane mainContainer;
     @FXML private AnchorPane results;
@@ -46,49 +46,62 @@ public class MainController implements Initializable{
 
         programPref = Preferences.userNodeForPackage(MainController.class);
         if(programPref.get("user", null) == null){
-            try {
-                FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("../FXML/loginregisterform.fxml"));
-                Parent login = loginLoader.load();
-                String cssDoc = getClass().getResource("../CSS/custom.css").toExternalForm();
-                login.getStylesheets().add(cssDoc);
-
-                LoginController loginController = loginLoader.getController();
-                loginController.setMainController(this);
-
-                Stage stage = new Stage();
-                stage.setTitle("Login or Signup");
-                stage.setScene(new Scene(login));
-                stage.showAndWait();
-
-                System.out.println("The email is " + userController.getUser().getEmail() + "\nThe password is: " + userController.getUser().getPassword());
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Error: Unable to Access Login Page");
+            try{
+                loadLogin();
+            }catch (IOException exception){
+                exception.printStackTrace();
+                System.out.println("Unable to load login page.");
             }
         }else{
             System.out.println("OK it is persisiting information now");
         }
 
-        loadFXML();
+        user = userController.getUser();
+
+        try {
+            loadPages();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadLogin() throws IOException{
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("../FXML/loginregisterform.fxml"));
+        Parent login = loginLoader.load();
+        String cssDoc = getClass().getResource("../CSS/custom.css").toExternalForm();
+        login.getStylesheets().add(cssDoc);
+
+        LoginController loginController = loginLoader.getController();
+        loginController.setMainController(this);
+
+        Stage stage = new Stage();
+        stage.setTitle("Login or Signup");
+        stage.setScene(new Scene(login));
+        stage.showAndWait();
+
+        System.out.println("The email is " + userController.getUser().getEmail() +
+                "\nThe password is: " + userController.getUser().getPassword());
 
     }
 
-    private void loadFXML(){
-        try {
+    private void loadPages() throws IOException{
+
+        settings = FXMLLoader.load(getClass().getResource("../FXML/settings.fxml"));
+
+        FXMLLoader searchLoader = new FXMLLoader(getClass().getResource("../FXML/search.fxml"));
+        search = searchLoader.load();
+        SearchController searchController = searchLoader.getController();
+        searchController.setMainController(this);
+
+        if(user.getUserType() != User.UserType.COLA_USER){
             results = FXMLLoader.load(getClass().getResource("../FXML/results.fxml"));
             applications = FXMLLoader.load(getClass().getResource("../FXML/applications.fxml"));
             form =  FXMLLoader.load(getClass().getResource("../FXML/form.fxml"));
-            settings = FXMLLoader.load(getClass().getResource("../FXML/settings.fxml"));
-
-            FXMLLoader searchLoader = new FXMLLoader(getClass().getResource("../FXML/search.fxml"));
-            search = searchLoader.load();
-            SearchController searchController = searchLoader.getController();
-            searchController.setMainController(this);
 
             mainContainer.getChildren().setAll(applications);
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        }else {
+            mainContainer.getChildren().setAll(search);
         }
     }
 
@@ -119,4 +132,7 @@ public class MainController implements Initializable{
         return this.programPref;
     }
 
+    public User getUser(){
+        return this.user;
+    }
 }
